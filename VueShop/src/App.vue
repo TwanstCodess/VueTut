9b6a1114fa385085346a26382b5d990886abfe65
Twanst Codes <155393812+TwanstCodess@users.noreply.github.com>
2024-03-16 13:17:06 +0300

    LifeCylce Hooks

--- VueShop/src/App.vue
+++ VueShop/src/App.vue
@@ -1,16 +1,24 @@
 <script setup>
-import data from "./data.json";
+import { onBeforeMount, onMounted, onUpdated, ref } from "vue";
+
+const counter = ref(0);
+
+onBeforeMount(() => {
+  console.log("Before Mounted");
+});
+
+onMounted(() => {
+  console.log("One Mounted");
+});
+
+onUpdated(() => {
+  console.log("On Update");
+});
 </script>
 
 <template>
   <div class="flex flex-col items-center justify-center h-screen gap-y-3">
-    <h1 class="text-4xl font-bold">List Render</h1>
-    <div
-      v-for="i in data"
-      :key="i"
-      class="bg-slate-300 p-10 w-1/2 text-center hover:shadow-xl rounded-2xl"
-    >
-      {{ i.email }}
-    </div>
+    <h1 class="text-4xl font-bold">Couter</h1>
+    <h1 v-on:click="() => counter++">{{ counter }}</h1>
   </div>
 </template>
