2714749f07962f91ff87532f588d53bd87e6190a
Twanst Codes <155393812+TwanstCodess@users.noreply.github.com>
2024-03-16 15:24:36 +0300

    easy code

--- VueShop/src/pages/products.vue
+++ VueShop/src/pages/products.vue
@@ -1,16 +1,11 @@
-<script setup>
-import { useRouter } from "vue-router";
-
-const router = useRouter();
-function goToProduct(productid) {
-  router.push({ name: "product", params: { id: productid } });
-}
-</script>
+<script setup></script>
 
 <template>
   <div class="flex items-center justify-center h-full gap-4 bg-gray-500">
     <div v-for="n in 10" :key="n">
-      <button @click="goToProduct(n)">{{ n }}</button>
+      <button>
+        <router-link :to="'/products/' + n">{{ n }}</router-link>
+      </button>
     </div>
   </div>
 </template>
