86addcf975c6842d2849c86a08a19a9c43bd20ac
Twanst Codes <155393812+TwanstCodess@users.noreply.github.com>
2024-03-16 16:30:55 +0300

    get api and pagination

--- VueShop/src/pages/home.vue
+++ VueShop/src/pages/home.vue
@@ -1,5 +1,28 @@
+<script setup>
+import { onMounted, ref } from "vue";
+
+const randomuser = ref(null);
+
+async function GetDataUser(pageNumber, $id) {
+  try {
+    const response = await fetch(
+      `http://127.0.0.1:8000/api/users?page=${pageNumber}`
+    );
+    const data = await response.json();
+    randomuser.value = data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+var pages = 1;
+onMounted(async () => {
+  await GetDataUser(pages); // Fetch data from page 2
+});
+</script>
+
 <template>
   <div class="flex items-center justify-center h-full bg-gray-500">
-    this is Home Page
+    {{ randomuser }}
   </div>
+  <button v-on:click="GetDataUser((pages += 1))">Next</button>
 </template>
