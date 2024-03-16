66a44e7ca7b8f35196f445583eee6674d4a499d6
Twanst Codes <155393812+TwanstCodess@users.noreply.github.com>
2024-03-16 16:55:02 +0300

    Api

--- VueShop/src/pages/home.vue
+++ VueShop/src/pages/home.vue
@@ -3,26 +3,28 @@ import { onMounted, ref } from "vue";
 
 const randomuser = ref(null);
 
-async function GetDataUser(pageNumber, $id) {
+async function GetDataUser() {
   try {
-    const response = await fetch(
-      `http://127.0.0.1:8000/api/users?page=${pageNumber}`
-    );
+    const response = await fetch("http://127.0.0.1:8000/api/users");
     const data = await response.json();
-    randomuser.value = data;
+    randomuser.value = data.data[0];
   } catch (error) {
     console.error("Error fetching data:", error);
   }
 }
-var pages = 1;
+
 onMounted(async () => {
-  await GetDataUser(pages); // Fetch data from page 2
+  await GetDataUser();
 });
 </script>
 
 <template>
-  <div class="flex items-center justify-center h-full bg-gray-500">
-    {{ randomuser }}
+  <div
+    class="flex items-center justify-center h-full text-3xl text-center bg-gray-500"
+  >
+    <div v-if="randomuser != null" class="flex flex-col text-center">
+      <h1>{{ randomuser.name }}</h1>
+      <h1>{{ randomuser.email }}</h1>
+    </div>
   </div>
-  <button v-on:click="GetDataUser((pages += 1))">Next</button>
 </template>
