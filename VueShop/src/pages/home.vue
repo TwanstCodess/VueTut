1a7ab717564bca47fa1ad380854a154043e515bc
Twanst Codes <155393812+TwanstCodess@users.noreply.github.com>
2024-03-16 17:13:56 +0300

    List

--- VueShop/src/pages/home.vue
+++ VueShop/src/pages/home.vue
@@ -7,7 +7,7 @@ async function GetDataUser() {
   try {
     const response = await fetch("http://127.0.0.1:8000/api/users");
     const data = await response.json();
-    randomuser.value = data.data[0];
+    randomuser.value = data;
   } catch (error) {
     console.error("Error fetching data:", error);
   }
@@ -19,12 +19,14 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div
-    class="flex items-center justify-center h-full text-3xl text-center bg-gray-500"
-  >
-    <div v-if="randomuser != null" class="flex flex-col text-center">
-      <h1>{{ randomuser.name }}</h1>
-      <h1>{{ randomuser.email }}</h1>
+  <div class="flex flex-col items-center justify-center gap-y-3">
+    <div
+      v-for="(user, index) in randomuser.data"
+      :key="index"
+      class="w-full p-10 text-center bg-slate-300 hover:shadow-xl rounded-2xl"
+    >
+      <h1>{{ user.name }}</h1>
+      <h2>{{ user.email }}</h2>
     </div>
   </div>
 </template>
